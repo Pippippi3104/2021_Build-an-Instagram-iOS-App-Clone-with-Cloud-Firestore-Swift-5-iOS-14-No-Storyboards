10e6f88bdeba76e54bd792393e1a89d40eb849b4
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-02-28 00:14:11 +0900

    # note

---
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/ProfileController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Satoshi Shimamura Second on 2021/02/28.
+//
+
+import UIKit
+
+class ProfileController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPurple
+    }
+}
