7cb9566ecb5c9b337171846971913121406a8ea8
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-02-28 00:07:18 +0900

    # note

--- src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -5,4 +5,13 @@
 //  Created by Satoshi Shimamura Second on 2021/02/27.
 //
 
-import Foundation
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
