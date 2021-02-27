30e131a99924f06f5877e7219a2bebfa956a7bd2
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-02-27 23:55:56 +0900

    # note

---
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -0,0 +1,8 @@
+//
+//  MainTabController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Satoshi Shimamura Second on 2021/02/27.
+//
+
+import Foundation
---
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Satoshi Shimamura Second on 2021/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
