1572bc487b45b809e19672c4d65d77522b281ea8
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-03-13 12:31:55 +0900

    # note

--- src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/Profile/ProfileHeader.swift
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/Profile/ProfileHeader.swift
@@ -5,4 +5,21 @@
 //  Created by Satoshi Shimamura Second on 2021/03/13.
 //
 
-import Foundation
+import UIKit
+
+class ProfileHeader: UICollectionReusableView {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
