0247511671794975a0fd4121736a8512c628111f
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-03-13 17:58:38 +0900

    # note

---
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Satoshi Shimamura Second on 2021/03/13.
+//
+
+import Firebase
+
+let COLLECTION_USERS = Firestore.firestore().collection("users")
+
