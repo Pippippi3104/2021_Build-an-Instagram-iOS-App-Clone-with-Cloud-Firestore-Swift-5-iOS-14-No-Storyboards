04e778b35e09000f24d12e897b1c4e5f7073d929
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2021-03-16 16:37:18 +0900

    # note

--- src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/UploadPostController.swift
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/UploadPostController.swift
@@ -5,4 +5,37 @@
 //  Created by Satoshi Shimamura Second on 2021/03/16.
 //
 
-import Foundation
+import UIKit
+
+class UploadPostController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Actions
+    
+    @objc func didTapCancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func didTapDone() {
+        print("DEBUG: Share post here ...")
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationItem.title = "Upload Post"
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(didTapCancel))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share",  style: .done, target: self, action: #selector(didTapDone))
+    }
+    
+}
