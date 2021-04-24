77baff3e2119c05f9f68227ff38204505bb6fe05
Satoshi Shimamura <>
2021-04-24 20:24:40 +0900

    note

--- src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/FeedCell.swift
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/FeedCell.swift
@@ -20,15 +20,13 @@ class FeedCell: UICollectionViewCell {
         iv.contentMode = .scaleAspectFill
         iv.clipsToBounds = true
         iv.isUserInteractionEnabled = true
-        iv.backgroundColor = .systemPurple
-        iv.image = #imageLiteral(resourceName: "venom-7")
+        iv.backgroundColor = .lightGray
         return iv
     }()
     
     private lazy var usernameButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitleColor(.black, for: .normal)
-        button.setTitle("venom", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
         button.addTarget(self, action: #selector(didTapUsername), for: .touchUpInside)
         return button
@@ -66,7 +64,6 @@ class FeedCell: UICollectionViewCell {
     
     private let likeLabel: UILabel = {
         let label = UILabel()
-        label.text = "1 like"
         label.font = UIFont.boldSystemFont(ofSize: 13)
         label.textColor = .black
         return label
@@ -135,6 +132,11 @@ class FeedCell: UICollectionViewCell {
         
         captionLabel.text = viewModel.caption
         postImageView.sd_setImage(with: viewModel.imageUrl)
+        
+        profileImageView.sd_setImage(with: viewModel.userProfileImageUrl)
+        usernameButton.setTitle(viewModel.username, for: .normal)
+        
+        likeLabel.text = viewModel.likesLabelText
     }
     
     func configureActionButtons() {
