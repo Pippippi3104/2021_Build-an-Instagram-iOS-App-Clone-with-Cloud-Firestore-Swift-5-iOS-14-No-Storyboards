6f2819ce044aa64f7533a7b9d4a515defb3d582d
Satoshi Shimamura <>
2021-05-01 10:56:28 +0900

    note

---
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/API/CommentService.swift
@@ -0,0 +1,27 @@
+//
+//  CommentService.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by SatoshiShimamura on 2021/05/01.
+//
+
+import Firebase
+
+struct CommentService {
+    static func uploadComment(comment: String, postID: String, user: User,
+                              completion: @escaping(FirestoreCompletion)) {
+        
+        let data: [String: Any] = ["uid": user.uid,
+                                   "comment": comment,
+                                   "timestamp": Timestamp(date: Date()),
+                                   "username": user.username,
+                                   "profileImageUrl": user.profileImageUrl]
+        
+        COLLECTION_POSTS.document(postID).collection("comments")
+            .addDocument(data: data, completion: completion)
+    }
+    
+    static func fetchComments() {
+        
+    }
+}
