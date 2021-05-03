7d9a252f2cb2bc367a8cc4c8550eb73d24e01261
Satoshi Shimamura <>
2021-05-04 02:41:39 +0900

    note

--- src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/NotificationsController.swift
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/NotificationsController.swift
@@ -7,11 +7,42 @@
 
 import UIKit
 
-class NotificationsController: UITabBarController {
+private let reuseIdentifier = "NotificationCell"
+
+class NotificationsController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureTableView() {
+        view.backgroundColor = .white
+        navigationItem.title = "Notifications"
         
-        view.backgroundColor = .systemOrange
+        tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 80
+        tableView.separatorStyle = .none
+    }
+}
+
+extension NotificationsController {
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier,
+                                                 for: indexPath) as! NotificationCell
+        cell.backgroundColor = .white
+        return cell
     }
 }
