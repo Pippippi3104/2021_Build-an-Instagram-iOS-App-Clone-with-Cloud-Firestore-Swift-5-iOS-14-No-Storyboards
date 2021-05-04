151da0aac6cd973db517c16fe2f637d2b9493797
Satoshi Shimamura <>
2021-05-04 16:47:00 +0900

    note

--- src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/ProfileController.swift
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/ProfileController.swift
@@ -128,6 +128,8 @@ extension ProfileController: UICollectionViewDelegateFlowLayout {
 
 extension ProfileController: ProfileHeaderDelegate {
     func header(_ profileHeader: ProfileHeader, didTapActionButtonFor user: User) {
+        guard let tab = tabBarController as? MainTabController else { return }
+        guard let currentUser = tab.user else { return }
         
         if user.isCurrentUser {
             print("DEBUG: Show edit profile here...")
@@ -142,6 +144,8 @@ extension ProfileController: ProfileHeaderDelegate {
             UserService.follow(uid: user.uid) { error  in
                 self.user.isFollowed = true
                 self.collectionView.reloadData()
+                
+                NotificationService.uploadNotification(toUid: user.uid, fromUser: currentUser, type: .follow)
             }
         }
     }
