ec93f3404040761db3e419f370e2248954087742
Satoshi Shimamura <>
2021-05-04 10:19:17 +0900

    note

---
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/API/NotificationService.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by SatoshiShimamura on 2021/05/04.
+//
+
+import Firebase
+
+struct NotificationService {
+    static func uploadNotification(toUid uid: String, type: NotificationType, post: Post? = nil) {
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
+        guard uid != currentUid else { return }
+        
+        var data: [String: Any] = ["timestamp": Timestamp(date: Date()),
+                                   "uid": currentUid,
+                                   "type": type.rawValue]
+        
+        if let post = post {
+            data["postId"] = post.postId
+            data["postImageUrl"] = post.imageUrl
+        }
+        
+        COLLECTION_NOTIFICATIONS.document(uid).collection("user-notifications").addDocument(data: data)
+    }
+    
+    static func fetchNotifications() {
+        
+    }
+}
