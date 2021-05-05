e22ed3c1706faf95bfb47240d0538517937a5ba3
Satoshi Shimamura <>
2021-05-05 12:16:16 +0900

    note

--- src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
@@ -101,6 +101,7 @@ class LoginController: UIViewController {
     
     @objc func handleShowResetPassword() {
         let controller = ResetPasswordController()
+        controller.delegate = self
         navigationController?.pushViewController(controller, animated: true)
     }
     
@@ -144,5 +145,14 @@ extension LoginController: FormViewModel {
         loginButton.backgroundColor = viewModel.buttonBackgroundColor
         loginButton.setTitleColor(viewModel.buttonTitleColor, for: .normal)
         loginButton.isEnabled = viewModel.formIsValid
+    }
+}
+
+// MARK: - ResetPasswordControllerDelegate
+
+extension LoginController: ResetPasswordControllerDelegate {
+    func controllerDidSendResetPasswordLink(_ controller: ResetPasswordController) {
+        navigationController?.popViewController(animated: true)
+        showMessage(withTitle: "Success", message: "We sent a link to your email to reset your password")
     }
 }
